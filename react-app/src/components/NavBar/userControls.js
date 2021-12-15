89ebeb02943f9f4ffeba34b0a26c124743750ca1
will-short <willofmetal85@gmail.com>
2021-12-15 12:02:10 -0600

    image dropdown

--- react-app/src/components/NavBar/userControls.js
+++ react-app/src/components/NavBar/userControls.js
@@ -5,9 +5,21 @@ import { useSelector } from "react-redux";
 import { useState } from "react";
 
 import UserDropdown from "./userDropdown";
+import dropDownStyle from "./dropDownStyle.module.css";
+import { useEffect } from "react";
+
 export default function UserControls({ setModal }) {
   const [toggleUserInfo, setToggleUserInfo] = useState(true);
   const session = useSelector((state) => state.session);
+
+  function handleBlur(e) {
+    // if the blur was because of outside focus
+    // currentTarget is the parent element, relatedTarget is the clicked element
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setToggleUserInfo(false);
+    }
+  }
+
   if (session?.user) {
     return (
       <>
@@ -41,10 +53,16 @@ export default function UserControls({ setModal }) {
         <button
           className={style.user}
           onClick={() => setToggleUserInfo(!toggleUserInfo)}
+          // onBlur={handleBlur}
         >
           <img src={session?.user.image_url} alt="" />
           <span className="material-icons">arrow_drop_down</span>
-          {toggleUserInfo && <UserDropdown />}
+          {toggleUserInfo && (
+            <UserDropdown
+              toggleUserInfo={toggleUserInfo}
+              setToggleUserInfo={setToggleUserInfo}
+            />
+          )}
         </button>
         <button className={style.cart}>
           <span className="material-icons">shopping_cart</span>
--- react-app/src/components/NavBar/userDropdown.js
+++ react-app/src/components/NavBar/userDropdown.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, NavLink, Route, Switch } from "react-router-dom";
 import style from "./navbar.module.css";
-import { useSelector } from "react-redux";
+import dropDownStyle from "./dropDownStyle.module.css";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
+import { logout } from "../../store/session";
 
 export default function UserDropdown() {
-  return <div className={style.userDropdown}></div>;
+  const dispatch = useDispatch();
+  const session = useSelector((state) => state.session);
+
+  return (
+    <div className={dropDownStyle.userDropdown}>
+      <Link className={dropDownStyle.user} to="/profile">
+        <img src={session.user.image_url} alt="" />
+        <h3>{session.user.username}</h3>
+        <p>View your profile</p>
+      </Link>
+      <button
+        className={dropDownStyle.logout}
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        <span class="material-icons-outlined">logout</span>
+        <span>Sign out</span>
+      </button>
+    </div>
+  );
 }
